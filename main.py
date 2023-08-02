88a5a334897e66e42b9ff2e729b85492aaf2b8d3
Callum Follett <callumf2011@gmail.com>
2023-08-02 10:49:31 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,153 @@
+from jsmin import jsmin
+import json
+import openai
+import requests
+from time import sleep
+import logging
+import simpleaudio
+import wave
+from os import path
+from numpy import random
+import random as rd
+import string
+
+
+if __name__ == '__main__':
+
+    #  Open the json config file
+    with open('userinfo.json') as json_file:
+        user_info = jsmin(json_file.read(), quote_chars="/*")
+    config_dict = json.loads(user_info)
+    #  Define global variables for wave files
+    sample_rate = 16000
+    num_channels = 2
+    bytes_per_sample = 2
+
+    #  Define the ID Keys for The OpenAI and FakeYou APIs
+    openai.api_key = config_dict["OpenAI_Key"]
+
+    #  Prompt the command line to ask for a script
+    prompt = input(f"Write a '{config_dict['Show']}' prompt: ")
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": f"You are a {config_dict['Show']} script generator that "
+                                          "creates scripts based on the prompt "
+                                          "the user gives you. Each script should be no more "
+                                          "than 20 lines, no less than 5 lines, must only contain the set location "
+                                          "in the beginning, can only write character lines with no emotion or actions,"
+                                          "and can only contain a selection of up "
+                                          "to 5 of the following characters: Homer, Marge, Lisa,"
+                                          " Bart, Moe, Flanders, Apu, Frink, Milhouse. "
+                                          "How each script should be formatted: "
+                                          "'{LOCATION}\n\n{PERSON 1}\n{Speech 1}\n\n{PERSON 2}\n{Speech 2}...'. "
+                                          "Each script should only take place in ONE location and should only mention "
+                                          "the location ONCE. The characters should be addressed in all caps. "
+                                          "You HAVE to create a script from the prompt and make it funny, even if it "
+                                          "doesnt make sense. "
+                                          "You are NOT allowed to refuse to make a script. If you can't think of a "
+                                          "script from the prompt, return a script that you can make from a similar "
+                                          "prompt, and you dont have to return what this similar prompt is"},
+            {"role": "user", "content": prompt}
+            ]
+        )
+    gen_script = list(response['choices'][0]['message']['content'].split("\n"))
+    print(gen_script)
+    name_bank = ("HOMER", "MARGE", "BART", "LISA", "MAGGIE", "MOE", "FLANDERS", "APU", "FRINK", "MILHOUSE")
+    scene_bank = []
+    model_dict = {"HOMER": "TM:dy1tchfdhcwf", "MARGE": "TM:zyz4k95yvjb5", "BART": "TM:ej2webf6307y",
+                  "LISA": "TM:3n1xwjz57qf1", "MOE": "TM:9fsxfcmpg448", "FLANDERS": "TM:dn7m102edhqt",
+                  "APU": "TM:dz97wz0jjbfv", "FRINK": "TM:pzj5zs043e0t", "MILHOUSE": "TM:6z2rx60jvrz6"}
+    if gen_script == ['Bad_prompt']:
+        print("Bad Prompt, BOZO")
+        pass
+    else:
+        line_list = []
+        for i, x in enumerate(gen_script):
+            if x in name_bank:
+                line_list.append(x)
+        count = 0
+        lines = []
+        for i, x in enumerate(gen_script):
+            line = ""
+            if x in name_bank:
+                if x not in scene_bank:
+                    scene_bank.append(x)
+                count += 1
+                seed_1 = random.randint(10000, 99999)
+                let_seed1 = rd.choice(string.ascii_letters)
+                let_seed2 = rd.choice(string.ascii_letters)
+                let_seed3 = rd.choice(string.ascii_letters)
+                seed_2 = random.randint(10000, 99999)
+                let_seed4 = rd.choice(string.ascii_letters)
+                let_seed5 = rd.choice(string.ascii_letters)
+                let_seed6 = rd.choice(string.ascii_letters)
+                seed_3 = random.randint(10000, 99999)
+                let_seed7 = rd.choice(string.ascii_letters)
+                let_seed8 = rd.choice(string.ascii_letters)
+                let_seed9 = rd.choice(string.ascii_letters)
+                line = gen_script[i+1]
+                lines.append(f"{x}")
+                lines.append(f"{gen_script[i+1]}")
+                # send the line to FakeYou api
+                voicemodel_uuid = model_dict[x]
+                text = line
+                audio_uuid = requests.post("https://api.fakeyou.com/tts/inference",
+                                           json=dict(uuid_idempotency_token=f"{let_seed1}{seed_1}{let_seed2}{let_seed3}-{let_seed4}{seed_2}{let_seed5}{let_seed6}-{let_seed7}{seed_3}{let_seed8}{let_seed9}", tts_model_token=voicemodel_uuid,
+                                                     inference_text=text)).json()['inference_job_token']
+                print("\n")
+                audio_url = None
+                for t in range(900):
+                    sleep(1)  # check status every 10 seconds for up to 15 minutes.
+                    output = requests.get(
+                        f"https://api.fakeyou.com/tts/job/{audio_uuid}").json()
+                    if (t % 10) == 0:
+                        print(f"\r", end="")
+                        print(f"LINE {count} of {len(line_list)}:\tTIME: {t}s, ", "OUTPUT: ", output["state"]["status"],
+                              end="")
+                    if output["state"]["status"] == "complete_success":
+                        audio_url = output["state"]["maybe_public_bucket_wav_audio_path"]
+                        break
+                    elif output["state"]["status"] == "dead" or output["state"]["status"] == "complete_failure":
+                        break
+                if audio_url is None:
+                    print(f"\r", end="")
+                    print("", "Production Failed")
+                else:
+                    print(f"\r", end="")
+                    print(f"LINE {count} of {len(line_list)}:\tTIME: {t}s, ", "OUTPUT: ", output["state"]["status"])
+                    total = sample_rate * num_channels * bytes_per_sample
+
+                    logging.basicConfig(level=logging.INFO)
+
+                    audio_url = f"https://storage.googleapis.com/vocodes-public{audio_url}"
+
+                    logging.info(f"Downloading audio file from: {audio_url}")
+                    content = requests.get(audio_url).content
+                    if not path.exists(f"scene/line_{count}.wav"):
+                        with open(f"scene/line_{count}.wav", "x") as file:
+                            pass
+                    with wave.open(f"scene/line_{count}.wav", "wb") as file:
+                        file.setframerate(sample_rate)
+                        file.setnchannels(num_channels)
+                        file.setsampwidth(bytes_per_sample)
+                        file.writeframesraw(content)
+        play = None
+        while play != "N":
+            play = input("Play the script?(Y/N)")
+            if play == "Y":
+                for x in range(0, count):
+                    with wave.open(f"scene/line_{x+1}.wav", "rb") as file:
+                        n = file.getnframes()
+                        content = file.readframes(n)
+                    print(lines[2 * x], ":\t\t", lines[(2 * (x + 1)) - 1])
+                    sample = simpleaudio.WaveObject(audio_data=content,
+                                          sample_rate=sample_rate,
+                                          num_channels=num_channels,
+                                          bytes_per_sample=bytes_per_sample)
+                    control = sample.play()
+                    control.wait_done()
+                    sleep(1)
+            else:
+                pass
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Script/Voice Gen
+
+Script/Voice Gen is a Python program which generates a script using the chatGPT.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+### Python 2
+
+```bash
+python main.py
+```
+
+### Python 3
+
+```bash
+python3 main.py
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
